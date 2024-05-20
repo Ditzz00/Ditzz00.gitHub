51230e58c4c6b22cfabf0e9ba9749d51026bc296
Ditzz00 <adityax01x02@gmail.com>
2024-05-20 21:09:29 +0800

    update kode dengan kode baru

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
-document.getElementById('gambar1').addEventListener('mouseover', function() {
-    this.style.transform = "scale(1.5)";
-    this.style.transition = "transform 0.5s ease"; 
-    this.style.backgroundColor = "#e71111"; 
-});
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let section = document.querySelector('section');
+let navLinks = document.querySelector('header nav a');
 
-document.getElementById('gambar1').addEventListener('mouseout', function() {
-    this.style.transform = "scale(1)";
-    this.style.transition = "transform 0.5s ease"; 
-    this.style.backgroundColor = "transparent"; 
-});
 
-document.querySelector('.profil.ml img').addEventListener('mouseover', function() {
-    this.style.transform = "scale(1.5)";
-    this.style.transition = "transform 0.5s ease"; 
-    this.style.backgroundColor = "#e71111"; 
-});
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-document.querySelector('.profil.ml img').addEventListener('mouseout', function() {
-    this.style.transform = "scale(1)";
-    this.style.transition = "transform 0.5s ease"; 
-    this.style.backgroundColor = "transparent"; 
-});
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' + id + ']').
+                classList.add('active');
+            })
+        }
+    })
+}
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
